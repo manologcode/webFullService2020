a4ee9aca2d816276765dfb01527bc806d475adfa
Manuel Gomez Mateo <creawww@gmail.com>
2021-06-03 08:35:57 +0200

    small changes in send email ajax

--- js/ajaxmail.js
+++ js/ajaxmail.js
@@ -2,23 +2,19 @@ $(document).ready(function(){
 	$('#contactForm').submit(function(event){
 		event.preventDefault();
     $("#resultados").html('Enviando...')
-
 		$.ajax({
 			type: 'POST',
 			url: 'https://email.creawww.es',
 			data: $(this).serialize(),
 			success: function(data){
-        var obj = jQuery.parseJSON( data );
-        if (obj.status == "OK"){
-          document.getElementById('contactForm').reset();
-        }
-		console.log(obj.response)
-		console.log(obj.status)
-        $("#resultados").html(obj.response)
+				if (data.status == "OK"){
+					document.getElementById('contactForm').reset();
+				}
+				$("#resultados").html(data.response)
 			},
 			error: function(data){
-        $("#resultados").html(data)
+				$("#resultados").html(data.response)
 			}
+			})
 		})
-	})
 });
